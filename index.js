d767efe4937bb08a09aa34ccd03715c5e02740ab
farhan-nahid <nahidahmed9933@gmail.com>
2021-08-10 01:38:10 +0600

    event done

---
+++ index.js
@@ -0,0 +1,42 @@
+const counters = document.querySelectorAll(".counter12364");
+const speed = 200;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    let inc = target / speed;
+
+    if (count < target) {
+      counter.innerText = count + inc;
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+
+  updateCount();
+});
+
+//Get the button
+var myButton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
